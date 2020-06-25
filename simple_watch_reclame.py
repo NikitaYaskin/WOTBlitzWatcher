15df3b864c1a9ebafa90b1be300307b3091e60d5
Nikita <nikuuu66@gmail.com>
2020-06-25 16:54:16 +0300

    Add loading logins from file
    Now logins and password will load from file logins.txt, with dictionary structure.

--- simple_watch_reclame.py
+++ simple_watch_reclame.py
@@ -1,8 +1,16 @@
 import pyautogui as pag
-import time, random, logging, datetime
+import time, random, logging, datetime, ast
 
 logging.basicConfig(filename='blitz.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
 logging.info('Started')
+
+def load_logins():
+	logging.info("Load logins from logins.txt file")
+	file = open("logins.txt", "r")
+	contents = file.read()
+	users = ast.literal_eval(contents)
+	file.close()
+	return users
 
 def close_video():
     exit_button = (1400, 82)
@@ -13,9 +21,9 @@ def close_video():
 def back_button():
     pag.click(51, 75)
 
-def play_video(delay):
+def play_video(cordinates, delay):
 	logging.info("Start plaing video")
-	video_button = (1373, 314)
+	video_button = (cordinates)
 	play_button = (859, 616)
 
 	logging.info("Click on play video button")
@@ -24,9 +32,9 @@ def play_video(delay):
 
 	logging.info("Confirm to play video")
 	pag.click(play_button[0], play_button[1])
-	time.sleep(45)
+	time.sleep(35)
 
-	logging.info("Close video after 45 sec delay")
+	logging.info("Close video after 35 sec delay")
 	close_video()
 
 def xp(delay):
@@ -74,18 +82,20 @@ def login(login, password, delay):
 	pag.click(989, 320, delay)
 
 	logging.info("Enter login")
-	pag.moveTo(721, 325, delay)
-	pag.click()
+	time.sleep(delay)
+	pag.click(721, 325)
 	pag.typewrite(login) 
 
 	logging.info("Enter password")
-	pag.moveTo(721, 410, delay)
-	pag.click()
+	time.sleep(delay)
+	pag.click(721, 410)
 	pag.typewrite(password) 
 
 	logging.info("Press Enter")
+	time.sleep(delay)
 	pag.press('enter')
-	time.sleep(10)
+	time.sleep(20)
+	back_button()
 
 def change_user(login_text, password_text, delay):
     
@@ -129,12 +139,17 @@ def count_users(user, users):
 		user += len(users[key])
 	return user
 
+def zeroing():
+	count, box = 1, 1
+
 count, box = 1, 1
-user, user_counter = 0, 0
+user, user_counter = 0, 2
 timedelay = 3
+x = False
 logins, passwords = [], []
-users = {"RU": {"mail1@mail.com": "password1", 
-				"mail2@gmail.com": "password2"}}
+
+users = load_logins()
+
 REGIONS = {"EU": [721, 354],"RU": [721, 438],"NA": [721, 530],"AS": [721, 619],"AC": [721, 687]} 
 
 reg = len(users.keys())
@@ -143,44 +158,54 @@ logging.info("There is " + str(reg) + " regions.")
 user = count_users(user, users)
 logging.info("There is " + str(user) + " accounts.")
 
-for i, y in users["RU"].items():
-	logins.append(i)
-	passwords.append(y)
+for r in users.keys():
+	for i, y in users[str(r)].items():
+		logins.append(i)
+		passwords.append(y)
 
-login(logins[user_counter], passwords[user_counter], timedelay)
-logging.info("Login to " + str(logins[user_counter]) + " user.")
+#login(logins[user_counter], passwords[user_counter], timedelay)
+#logging.info("Login to " + str(logins[user_counter]) + " user.")
 
-'''
 while True:
 	if count <= 5:
-        if count == 1 or count == 4:
-            open_box(timedelay)
-            if box == 1 or box == 2:
-                box += 1
-                logging.info("Open " + str(box) + " box.")
-        play_video(timedelay)
-        logging.info("Watch " + str(count) + " video.")
-        count += 1
-        time.sleep(random.randint(131, 135))
-        
-    elif count == 6:
-        xp(timedelay)
-        logging.info("Get ?? XP")
-        count += 1
-        if box == 3:
-        	time.sleep(random.randint(70, 100))
-        	open_box(timedelay)
-        box += 1
-        logging.info("Open " + str(box) + " box.")
+		if count == 1 or count == 4:
+			open_box(timedelay)
+			time.sleep(timedelay)
+			if count == 1:
+				logging.info("Start detecting video button")
+				while not x:
+					if pag.pixelMatchesColor(1423, 385 ,(62, 152, 15), tolerance=10) or pag.pixelMatchesColor(1423, 385 ,(63, 155, 16), tolerance=10):
+						x = (1423, 385)
+						logging.info("Video button detected in upper position")
+					elif pag.pixelMatchesColor(1425, 273 ,(62, 154, 15), tolerance=10):
+						x = (1425, 273)
+						logging.info("Video button detected in lower position")
+					time.sleep(10)
+			if box == 1 or box == 2:
+				box += 1
+				logging.info("Open " + str(box) + " box.")
+		play_video(x, timedelay)
+		logging.info("Watch " + str(count) + " video.")
+		count += 1
+		time.sleep(random.randint(131, 135))
 
-    elif count == 7:
-    	if user_counter == user:
-    		logout(timedelay)
-    		break
+	elif count == 6:
+		xp(timedelay)
+		logging.info("Get ?? XP")
+		count += 1
+		if box == 3:
+			time.sleep(random.randint(70, 100))
+			open_box(timedelay)
+		box += 1
+		logging.info("Open " + str(box) + " box.")
 
-    	user_counter += 1
-    	change_user(logins[user_counter], passwords[user_counter], timedelay)
-    	#TypeError: 'str' object is not callable
-    	logging.info("Change user to " + str(logins[user_counter]))
-    	count = 1
-    	pag.click(65, 94)'''
+	elif count == 7:
+		user_counter += 1
+		if user_counter == user:
+			logout(timedelay)
+			break
+
+		change_user(logins[user_counter], passwords[user_counter], timedelay)
+		logging.info("Change user to " + str(logins[user_counter]))
+		count, box = 1, 1
+		pag.click(65, 94)
