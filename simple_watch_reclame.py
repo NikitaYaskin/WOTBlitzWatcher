1fc515daf128f1e59c87512284a2ea545e191d08
Nikita <nikuuu66@gmail.com>
2020-06-27 14:38:52 +0300

    Add timer per account

--- simple_watch_reclame.py
+++ simple_watch_reclame.py
@@ -15,14 +15,13 @@ def load_logins():
 
 def close_video():
 	"""Clicks cross button, to close video"""
-    exit_button = (1400, 82)
-    exit_button2 = (32, 77)
-
-    pag.click(exit_button[0], exit_button[1])
+	exit_button = (1400, 82)
+	exit_button2 = (32, 77)
+	pag.click(exit_button[0], exit_button[1])
 
 def back_button():
 	"""Clicks back button, to return to main menu"""
-    pag.click(51, 75)
+	pag.click(51, 75)
 
 def play_video(cordinates, delay):
 	"""Start playng the wideo, after 33 seconds close video"""
@@ -91,7 +90,7 @@ def login(login, password, delay):
 	logging.info("Enter login")
 	time.sleep(delay)
 	pag.click(721, 325)
-	pag.typewrite(login) 
+	pag.typewrite(login)
 
 	logging.info("Enter password")
 	time.sleep(delay)
@@ -103,6 +102,7 @@ def login(login, password, delay):
 	pag.press('enter')
 	time.sleep(20)
 	back_button()
+	time.sleep(10)
 
 def change_user(login_text, password_text, delay):
     
@@ -113,27 +113,27 @@ def change_user(login_text, password_text, delay):
 
 def open_box(delay):
 	"""Stand for every day opaning 3 first boxes"""
-    logging.info("Start opaning box")
-    pag.click(1381, 100)
+	logging.info("Start opaning box")
+	pag.click(1381, 100)
 
-    logging.info("Open menu")
-    time.sleep(delay)
-    
-    pag.click(1024, 158)#1318, 198)
-    logging.info("Open box menu")
-    time.sleep(delay)
-    
-    pag.click(229, 704)
-    logging.info("Open first box")
-    time.sleep(delay)
+	logging.info("Open menu")
+	time.sleep(delay)
 
-    back_button()
-    logging.info("Close opaned prise")
-    time.sleep(delay)
+	pag.click(1024, 160)#1318, 198)
+	logging.info("Open box menu")
+	time.sleep(delay)
 
-    back_button()
-    back_button()
-    logging.info("Back to main manu")
+	pag.click(229, 704)
+	logging.info("Open first box")
+	time.sleep(delay)
+
+	back_button()
+	logging.info("Close opaned prise")
+	time.sleep(delay)
+
+	back_button()
+	back_button()
+	logging.info("Back to main manu")
 
 def open_game():
     pag.hotkey('win', 's')
@@ -151,7 +151,8 @@ def zeroing():
 	count, box = 1, 1
 
 count, box = 1, 1
-user, user_counter = 0, 0
+user, user_counter = 0, 2
+
 timedelay = 2
 x = False
 logins, passwords = [], []
@@ -166,13 +167,16 @@ logging.info("There is " + str(reg) + " regions.")
 user = count_users(user, users)
 logging.info("There is " + str(user) + " accounts.")
 
-for r in users.keys():
-	for i, y in users[str(r)].items():
-		logins.append(i)
-		passwords.append(y)
+#for r in users.keys(): str(r)
+for i, y in users["RU"].items():
+	logins.append(i)
+	passwords.append(y)
 
-login(logins[user_counter], passwords[user_counter], timedelay)
-logging.info("Login to " + str(logins[user_counter]) + " user.")
+if count == 1:
+	login(logins[user_counter], passwords[user_counter], timedelay)
+	logging.info("Login to " + str(logins[user_counter]) + " user.")
+	startWatching = datetime.datetime.now()
+	print("Login to " + str(logins[user_counter]) + " user in " + str(startWatching.strftime("%Y-%m-%d %H:%M:%S")))
 
 while True:
 	if count <= 5:
@@ -183,37 +187,48 @@ while True:
 				logging.info("Start detecting video button")
 				while not x:
 					if pag.pixelMatchesColor(1423, 385 ,(62, 152, 15), tolerance=10) or pag.pixelMatchesColor(1423, 385 ,(63, 155, 16), tolerance=10):
-						x = (1423, 385)
+						x = (1420, 390)
 						logging.info("Video button detected in upper position")
+						print(1420, 390)
 					elif pag.pixelMatchesColor(1425, 273 ,(62, 154, 15), tolerance=10):
-						x = (1425, 273)
+						x = (1420, 282)
 						logging.info("Video button detected in lower position")
+						print(1420, 282)
 					time.sleep(10)
 			if box == 1 or box == 2:
-				box += 1
 				logging.info("Open " + str(box) + " box.")
+				print("Open " + str(box) + " box.")
+				box += 1
 		play_video(x, timedelay)
 		logging.info("Watch " + str(count) + " video.")
+		print("Watch " + str(count) + " video.")
 		count += 1
 		time.sleep(130)
 
 	elif count == 6:
 		xp(timedelay)
 		logging.info("Get ?? XP")
+		print("Get ?? XP")
 		count += 1
 		if box == 3:
 			time.sleep(80)
 			open_box(timedelay)
-		box += 1
 		logging.info("Open " + str(box) + " box.")
+		print("Open " + str(box) + " box.")
+		box += 1
 
 	elif count == 7:
 		user_counter += 1
+		endWatching = datetime.datetime.now()
+		timer = endWatching - startWatching
+		print("Watchin video in this account " + str(timer.minutes) + " minutes " + str(timer.seconds))
+		logging.info("Watchin video in this account " + str(timer.minutes) + " minutes " + str(timer.seconds))
 		if user_counter == user:
 			logout(timedelay)
 			break
 
 		change_user(logins[user_counter], passwords[user_counter], timedelay)
-		logging.info("Change user to " + str(logins[user_counter]))
+		logging.info("Change user to " + str(logins[user_counter]) + " in " + str(endWatching.strftime("%Y-%m-%d %H:%M:%S")))
+		print("Change user to " + str(logins[user_counter]))
 		count, box = 1, 1
-		pag.click(65, 94)
+		#pag.click(65, 94)
