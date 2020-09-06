46c3abb2cbf9629dc2a51b764594652c17a5453f
Nikita <nikuuu66@gmail.com>
2020-09-06 16:18:09 +0300

    Add files via upload

--- blitz_ads.py
+++ blitz_ads.py
@@ -6,14 +6,14 @@ logging.info('Started')
 
 def screenshot(login):
         time.sleep(2)
-        imgName = 'count/' + login + str(datetime.datetime.today()).replace(".", "--", 1).replace(":", "-", 1) + '.png'
+        imgName = 'count/' + login + str(datetime.datetime.today()).replace(".", " ", 1).replace(":", "-", 1) + '.png'
         pag.screenshot(imgName, region=(117, 50, 1197, 42))
 
 def check_if_main_menu():
         """Checking if on desplay main menu"""
-        if pag.locateOnScreen('img\Wboi1.png', confidence=0.9) == None:
+        if pag.locateOnScreen('img//Wboi1.png', confidence=0.9) == None:
                 back_button()
-                time.sleep(2)
+                time.sleep(3)
                 check_if_main_menu()
 
 def locate_video_button(videoButtonLocation):
@@ -165,9 +165,19 @@ def change_user(login_text, password_text, delay):
         #change_region( ,delay)
         login(login_text, password_text, delay)
 
-def open_box(delay):
+def open_medium_box(delay):
+        """Check second and third boxes"""
+        open_box_menu(delay)
+        for pos in pag.locateAllOnScreen('img//openBox.png', confidence=0.8) != None:
+                pag.click(pag.locateAllOnScreen('img//openBox.png', confidence=0.8))
+                time.sleep(delay)
+                back_button()
+
+def open_box(counter, delay):
         """Stand for every day opaning 3 first boxes"""
         open_box_menu(delay)
+        if counter == 1:
+                open_medium_box(delay)
 
         pag.click(229, 704)
         logging.info("Open first box")
@@ -192,24 +202,7 @@ def pixel_detection(delay, spot, colour):
                 if pag.pixelMatchesColor(spot[0], spot[1] ,(colour[0], colour[1], colour[2]), tolerance=10):
                         var = 1
                         return True
-
-def check_medium_box(delay, detection):
-        """Check second and third boxes"""
-        open_box_menu(delay)
-        boxes = {(836, 291): (725, 684), (1331, 291): (1213, 680)}
-        for item in boxes:
-                if pixel_detection(delay, item, (196, 33, 22)):
-                        detection = boxes[item]
-                        logging.info("Detect box")
-                        print(boxes[item])
-                        logging.info("Open box")
-                        pag.click(boxes[item])
-                        time.sleep(delay)
-                        back_button()
-                        back_button()
-                else:
-                        print('No box', str(item))
-                        break
+                
 
 count, box = 1, 1
 user, user_counter = 0, 0
@@ -240,7 +233,7 @@ if count == 1:
         login(logins[user_counter], passwords[user_counter], timedelay)
         logging.info("Login to " + str(logins[user_counter]) + " user.")
         print("Login to " + str(logins[user_counter]) + " user in ")
-        screenshot(logins[user_counter][:6])
+        screenshot(logins[user_counter][:5])
 
 
 while True:
@@ -251,19 +244,19 @@ while True:
                                 logging.info("Open " + str(box) + " box.")
                                 print("Open " + str(box) + " box.")
                                 box += 1
-                                open_box(timedelay)
-                                screenshot(logins[user_counter][:6])
+                                open_box(count, timedelay)
+                                screenshot(logins[user_counter][:5])
                         time.sleep(timedelay)
 
                         if count == 4:
                                 logging.info("Open " + str(box) + " box.")
                                 print("Open " + str(box) + " box.")
-                                open_box(timedelay)
-                                screenshot(logins[user_counter][:6])
+                                open_box(count, timedelay)
+                                screenshot(logins[user_counter][:5])
                                 box += 1
         
                 play_video(videoButtonLocation, timedelay)
-                screenshot(logins[user_counter][:6])
+                screenshot(logins[user_counter][:5])
                 logging.info("Watch " + str(count) + " video.")
                 print("Watch " + str(count) + " video.")
                 count += 1
@@ -271,27 +264,24 @@ while True:
 
         elif count == 6:
                 xp(timedelay)
-                screenshot(logins[user_counter][:6])
+                screenshot(logins[user_counter][:5])
                 logging.info("Get ?? XP")
                 print("Get ?? XP")
                 count += 1
                 
                 if box == 3:
                         time.sleep(90)
-                        open_box(timedelay)
-                        screenshot(logins[user_counter][:6])
+                        open_box(count, timedelay)
+                        screenshot(logins[user_counter][:5])
                         
-                logging.info("Open " + str(box) + " box.")
-                print("Open " + str(box) + " box.")
-                box += 1
+                        logging.info("Open " + str(box) + " box.")
+                        print("Open " + str(box) + " box.")
+                        box += 1
 
         elif count == 7:
                 user_counter += 1
                 time.sleep(timedelay)
-                if pag.pixelMatchesColor(1407, 65, (244, 244, 244), tolerance=20):
-                        check_medium_box(timedelay, boxDetection)
-                        
-                screenshot(logins[user_counter][:6])
+                screenshot(logins[user_counter][:5])
                 
                 if user_counter == user:
                         logout(timedelay)
